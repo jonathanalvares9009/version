be1a393b03b16e66ffa03e2c3162ff9222d51218
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-11-26 09:44:25 +0530

    Init function created

---
+++ version/index.js
@@ -0,0 +1,34 @@
+import { inRepo } from "../file/in-repo";
+import { writeFilesFromTree } from "../file/write-files-from-tree";
+import { objToStr } from "../config/obj-to-str";
+
+export const init = (opts) => {
+  // Abort if already a repository.
+  if (inRepo()) {
+    return;
+  }
+
+  opts = opts || {};
+
+  // Create a JS object that mirrors the Git basic directory structure.
+  const versionStructure = {
+    HEAD: "ref: refs/heads/master\n",
+
+    // If --bare was passed, write to the Git config indicating that the repository is bare.
+    // If --bare was not passed, write to the Git config saying the repository is not bare.
+    config: objToStr({ core: { "": { bare: opts.bare === true } } }),
+
+    objects: {},
+    refs: {
+      heads: {},
+    },
+  };
+
+  // Write the standard Git directory structure using the versionStructure JS object.
+  // If the repository is not bare, put the directories inside the .version directory.
+  // If the repository is bare, put them in the top level of the repository.
+  writeFilesFromTree(
+    opts.bare ? versionStructure : { ".version": versionStructure },
+    process.cwd()
+  );
+};
